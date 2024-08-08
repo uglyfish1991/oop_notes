9d28a610a79b7700c4574fe2a9e110c2a745cce6
Katy Long <katy.long@wearecodenation.com>
2024-08-08 11:04:43 +0100

    person class created

---
+++ main.py
@@ -0,0 +1,7 @@
+from person import Person
+
+katy = Person("Katy", 32, "average")
+
+print(katy.age)
+print(katy.name)
+print(katy.height)
--- person.py
+++ person.py
@@ -1,2 +1,8 @@
-#hjdfcshmjncdsvhjkdfasjlk;fad sjlk ;dfsa jkn dfskljfdsnkldvskn m
-# 
+class Person():
+    def __init__(self,person_name, person_age, person_height):
+        self.name = person_name
+        self.age = person_age
+        self.height = person_height
+        
+        
+
